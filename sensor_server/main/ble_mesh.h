6c6952c477701343d30e263c5c3cef9c973181f1
Tomasz Babiuk <tomasz.babiuk@assaabloy.com>
2024-02-09 20:49:14 +0100

    Extracting ble logic to ble_mesh.c

---
+++ sensor_server/main/ble_mesh.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "esp_ble_mesh_defs.h"
+#include "esp_ble_mesh_common_api.h"
+#include "esp_ble_mesh_networking_api.h"
+#include "esp_ble_mesh_provisioning_api.h"
+#include "esp_ble_mesh_config_model_api.h"
+#include "esp_ble_mesh_sensor_model_api.h"
+#include "esp_ble_mesh_time_scene_model_api.h"
+#include "esp_ble_mesh_health_model_api.h"
+
+#include "ble_mesh_example_init.h"
+
+#define CID_ESP     0x02E5
+
+#define BT_MESH_PROP_ID_PRESENT_INDOOR_AMB_TEMP             0x0056
+#define BT_MESH_PROP_ID_PRESENT_AMB_LIGHT_LEVEL             0x004E
+#define BT_MESH_PROP_ID_PRESENT_INDOOR_RELATIVE_HUMIDITY    0x00A7
+#define BT_MESH_PROP_ID_MOTION_SENSED                       0x0042
+
+#define SENSOR_POSITIVE_TOLERANCE   ESP_BLE_MESH_SENSOR_UNSPECIFIED_POS_TOLERANCE
+#define SENSOR_NEGATIVE_TOLERANCE   ESP_BLE_MESH_SENSOR_UNSPECIFIED_NEG_TOLERANCE
+#define SENSOR_SAMPLE_FUNCTION      ESP_BLE_MESH_SAMPLE_FUNC_UNSPECIFIED
+#define SENSOR_MEASURE_PERIOD       ESP_BLE_MESH_SENSOR_NOT_APPL_MEASURE_PERIOD
+#define SENSOR_UPDATE_INTERVAL      ESP_BLE_MESH_SENSOR_NOT_APPL_UPDATE_INTERVAL
+
+esp_err_t ble_mesh_init(void);
+
+void ble_mesh_publish_sensors_data();
+
+void ble_mesh_update_temperature(float new_value);
+
+void ble_mesh_update_humidity(float new_value);
+
+void ble_mesh_update_luminocity(float new_value);
